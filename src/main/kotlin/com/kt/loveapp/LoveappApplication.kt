ab535e4a2989f22bfea0a8408b0d41f166b5d9cf
Alberto Miranda Dencowski <allowski@gmail.com>
2022-03-03 13:38:39 -0300

    Initial commit

---
+++ src/main/kotlin/com/kt/loveapp/LoveappApplication.kt
@@ -0,0 +1,11 @@
+package com.kt.loveapp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LoveappApplication
+
+fun main(args: Array<String>) {
+	runApplication<LoveappApplication>(*args)
+}
---
+++ src/test/kotlin/com/kt/loveapp/LoveappApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kt.loveapp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LoveappApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
